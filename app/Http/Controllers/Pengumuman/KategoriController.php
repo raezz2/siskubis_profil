2d0547bd7b991eb9910a4e9823ce7d9dfb0d1974
ahmad <rn.choir47@gmail.com>
2020-09-14 16:06:35 +0700

    Create KategoriController.php

---
+++ app/Http/Controllers/Pengumuman/KategoriController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Pengumuman;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use App\Pengumuman;
+use App\Priority;
+use App\Inkubator;
+
+class KategoriController extends Controller
+{
+        public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    public function index($id){
+    }
+    public function kategori($id){
+        $pengumuman = Pengumuman::where('priority_id',$id)->latest()->get();
+        $kategori = DB::table('priority')->get();
+        return view('pengumuman.index',compact('pengumuman','kategori'));
+    }
+}
