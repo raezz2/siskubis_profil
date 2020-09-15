bfb9e07ff12425f8654b38ec2cc5204e5516ebdb
roodey21 <45893614+roodey21@users.noreply.github.com>
2020-09-15 16:04:22 +0700

    Update calendar.blade.php

--- resources/views/event/calendar.blade.php
+++ resources/views/event/calendar.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.app')
+
 @section('content')
 <div class="row">
 	<div class="col-md-3">
@@ -14,7 +15,6 @@
 				</div>
 			  </div>
 			</div>
-			@role('inkubator')
 			<div class="card-body">
 				<div class="create_event_wrap">
 					<form class="js-form-add-event">
@@ -25,19 +25,19 @@
 					</form>
 					<ul class="list-group" id="external-events">
 						<li class="list-group-item bg-success fc-event">
-							Scale Up
+							Hello World
 
 						</li>
 						<li class="list-group-item bg-primary fc-event">
-							Proposal
+							Go to Shopping
 
 						</li>
 						<li class="list-group-item bg-warning fc-event">
-							Start Up
+							Payment schedule
 
 						</li>
 						<li class="list-group-item bg-danger fc-event">
-							Pra Start Up
+							Rent Due
 
 						</li>
 					</ul>
@@ -47,10 +47,10 @@
 					</p>
 				</div>
 			</div>
-			@endrole
 		</div>
 	</div>
 	<div class="col-md-9">
+		<div class="response"></div>
 		<div class="card mb-4 o-hidden">
 			<div class="card-body">
 				<div id="calendar"></div>
@@ -63,23 +63,82 @@
     <link rel="stylesheet" href="{{ asset('theme/css/plugins/calendar/fullcalendar.min.css')}}" />
 @endsection
 @section('js')
-    <script src="{{ asset('theme/js/plugins/calendar/jquery-ui.min.js')}}"></script>
-    <script src="{{ asset('theme/js/plugins/calendar/moment.min.js')}}"></script>
-    <script src="{{ asset('theme/js/plugins/calendar/fullcalendar.min.js')}}"></script>
-	<script src="{{ asset('theme/js/scripts/calendar.script.min.js')}}"></script>
-	
-	<script>
-		var calendar = new Calendar(calendarEl, {
+<script src="{{ asset('theme/js/plugins/calendar/jquery-ui.min.js')}}"></script>
+<script src="{{ asset('theme/js/plugins/calendar/moment.min.js')}}"></script>
+<script src="{{ asset('theme/js/plugins/calendar/fullcalendar.min.js')}}"></script>
+{{-- <script src="{{ asset('theme/js/scripts/calendar.script.min.js')}}"></script> --}}
 
-			eventClick: function(info) {
-			alert('Event: ' + info.event.title);
-			alert('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
-			alert('View: ' + info.view.type);
+<script>
+	$(document).ready(function () {
 
-			// change the border color just for fun
-			info.el.style.borderColor = 'red';
-			}
+/* initialize the external events
+		-----------------------------------------------------------------*/
+function initEvent() {
+  $('#external-events .fc-event').each(function () {
+	// store data so the calendar knows to render an event upon drop
+	$(this).data('event', {
+	  title: $.trim($(this).text()),
+	  // use the element's text as the event title
+	  color: $(this).css('background-color'),
+	  stick: true // maintain when user navigates (see docs on the renderEvent method)
 
+	}); // make the event draggable using jQuery UI
+
+	$(this).draggable({
+	  zIndex: 999,
+	  revert: true,
+	  // will cause the event to go back to its
+	  revertDuration: 0 // original position after the drag
+
+	});
+  });
+}
+
+initEvent();
+/* initialize the calendar
+-----------------------------------------------------------------*/
+
+var newDate = new Date(),
+	date = newDate.getDate(),
+	month = newDate.getMonth(),
+	year = newDate.getFullYear();
+$('#calendar').fullCalendar({
+  header: {
+	left: 'prev,next today',
+	center: 'title',
+	right: 'month,agendaWeek,agendaDay'
+  },
+  themeSystem: "bootstrap4",
+  droppable: true,
+  editable: true,
+  eventLimit: true,
+  // allow "more" link when too many events
+  drop: function drop() {
+	// is the "remove after drop" checkbox checked?
+	if ($('#drop-remove').is(':checked')) {
+	  // if so, remove the element from the "Draggable Events" list
+	  $(this).remove();
+	}
+  },
+  events: [
+	  @foreach($event as $e){
+		title: "{{ $e->title }}",
+		start: "{{ $e->tgl_mulai }}",
+		end: "{{ $e->tgl_selesai }}",
+		color: "#ffc107",
+		url : "{{ $e->slug }}",
+	  },
+	  @endforeach
+  ]
 });
-	</script>
+jQuery(".js-form-add-event").on("submit", function (e) {
+  e.preventDefault();
+  var data = $('#newEvent').val();
+  $('#newEvent').val('');
+  $('#external-events').prepend('<li class="list-group-item bg-success fc-event">' + data + '</li>');
+  initEvent();
+});
+});
+</script>
+
 @endsection
