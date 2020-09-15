21c7ca4a7dc3b1789abf44935f9020b2a9baa407
roodey21 <fahrudin.abadi@students.amikom.ac.id>
2020-09-15 16:00:26 +0700

    tampil kalender

--- resources/views/event/calendar.blade.php
+++ resources/views/event/calendar.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.app')
+
 @section('content')
 <div class="row">
 	<div class="col-md-3">
@@ -49,6 +50,7 @@
 		</div>
 	</div>
 	<div class="col-md-9">
+		<div class="response"></div>
 		<div class="card mb-4 o-hidden">
 			<div class="card-body">
 				<div id="calendar"></div>
@@ -61,8 +63,82 @@
     <link rel="stylesheet" href="{{ asset('theme/css/plugins/calendar/fullcalendar.min.css')}}" />
 @endsection
 @section('js')
-    <script src="{{ asset('theme/js/plugins/calendar/jquery-ui.min.js')}}"></script>
-    <script src="{{ asset('theme/js/plugins/calendar/moment.min.js')}}"></script>
-    <script src="{{ asset('theme/js/plugins/calendar/fullcalendar.min.js')}}"></script>
-    <script src="{{ asset('theme/js/scripts/calendar.script.min.js')}}"></script>
+<script src="{{ asset('theme/js/plugins/calendar/jquery-ui.min.js')}}"></script>
+<script src="{{ asset('theme/js/plugins/calendar/moment.min.js')}}"></script>
+<script src="{{ asset('theme/js/plugins/calendar/fullcalendar.min.js')}}"></script>
+{{-- <script src="{{ asset('theme/js/scripts/calendar.script.min.js')}}"></script> --}}
+
+<script>
+	$(document).ready(function () {
+
+/* initialize the external events
+		-----------------------------------------------------------------*/
+function initEvent() {
+  $('#external-events .fc-event').each(function () {
+	// store data so the calendar knows to render an event upon drop
+	$(this).data('event', {
+	  title: $.trim($(this).text()),
+	  // use the element's text as the event title
+	  color: $(this).css('background-color'),
+	  stick: true // maintain when user navigates (see docs on the renderEvent method)
+
+	}); // make the event draggable using jQuery UI
+
+	$(this).draggable({
+	  zIndex: 999,
+	  revert: true,
+	  // will cause the event to go back to its
+	  revertDuration: 0 // original position after the drag
+
+	});
+  });
+}
+
+initEvent();
+/* initialize the calendar
+-----------------------------------------------------------------*/
+
+var newDate = new Date(),
+	date = newDate.getDate(),
+	month = newDate.getMonth(),
+	year = newDate.getFullYear();
+$('#calendar').fullCalendar({
+  header: {
+	left: 'prev,next today',
+	center: 'title',
+	right: 'month,agendaWeek,agendaDay'
+  },
+  themeSystem: "bootstrap4",
+  droppable: true,
+  editable: true,
+  eventLimit: true,
+  // allow "more" link when too many events
+  drop: function drop() {
+	// is the "remove after drop" checkbox checked?
+	if ($('#drop-remove').is(':checked')) {
+	  // if so, remove the element from the "Draggable Events" list
+	  $(this).remove();
+	}
+  },
+  events: [
+	  @foreach($event as $e){
+		title: "{{ $e->title }}",
+		start: "{{ $e->tgl_mulai }}",
+		end: "{{ $e->tgl_selesai }}",
+		color: "#ffc107",
+		url : "{{ $e->slug }}",
+	  },
+	  @endforeach
+  ]
+});
+jQuery(".js-form-add-event").on("submit", function (e) {
+  e.preventDefault();
+  var data = $('#newEvent').val();
+  $('#newEvent').val('');
+  $('#external-events').prepend('<li class="list-group-item bg-success fc-event">' + data + '</li>');
+  initEvent();
+});
+});
+</script>
+
 @endsection
