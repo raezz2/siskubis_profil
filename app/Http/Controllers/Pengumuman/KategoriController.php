a58704132d2e6e3d3db6989c93acc4f2bb1dbc53
ahmad <rn.choir47@gmail.com>
2020-09-22 16:20:51 +0700

    tambahan fitur kategori

--- app/Http/Controllers/Pengumuman/KategoriController.php
+++ app/Http/Controllers/Pengumuman/KategoriController.php
@@ -16,7 +16,12 @@ class KategoriController extends Controller
     {
         $this->middleware('auth');
     }
-    public function index($id){
+    public function index(){
+        $pengumuman = Pengumuman::where('author_id', \Auth::user()->id)->get();
+        $kategori = DB::table('priority')->get();
+        $inkubator = DB::table('inkubator')->get();
+        return view('pengumuman.index', compact('pengumuman', 'kategori', 'inkubator'));
+
     }
     public function kategori($id){
         $pengumuman = Pengumuman::where([['priority_id',$id],['author_id',\Auth::user()->id]])->latest()->get();
