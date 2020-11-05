46f9a9be73b253317a483e63a471d5c7388403a3
afan4212 <70931797+afan4212@users.noreply.github.com>
2020-11-05 15:35:58 +0700

    membuat detail tenant

--- resources/views/tenant/Start Up.blade.php
+++ resources/views/tenant/Start Up.blade.php
@@ -117,44 +117,43 @@
 				</ul>
 			</div>
 			<div class="tab-pane fade active show" id="about" role="tabpanel" aria-labelledby="timeline-tab">
-			<h4>Personal Information</h4>
+			<h4>Tenant Information</h4>
 				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, commodi quam! Provident quis voluptate asperiores ullam, quidem odio pariatur. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem, nulla eos?
-					Cum non ex voluptate corporis id asperiores doloribus dignissimos blanditiis iusto qui repellendus deleniti aliquam, vel quae eligendi explicabo.
+					{{$tenant->description}}
 				</p>
 				<hr />
 				<div class="row">
 					<div class="col-md-4 col-6">
 						<div class="mb-4">
-							<p class="text-primary mb-1"><i class="i-Calendar text-16 mr-1"></i> Birth Date</p><span>1 Jan, 1994</span>
+							<p class="text-primary mb-1"><i class="i-Calendar text-16 mr-1"></i> Birth Date</p><span>{{$tenant->tanggal_berdiri}}</span>
 						</div>
 						<div class="mb-4">
-							<p class="text-primary mb-1"><i class="i-Edit-Map text-16 mr-1"></i> Birth Place</p><span>Dhaka, DB</span>
+							<p class="text-primary mb-1"><i class="i-Edit-Map text-16 mr-1"></i> Alamat</p><span>{{$tenant->alamat}}</span>
 						</div>
 						<div class="mb-4">
-							<p class="text-primary mb-1"><i class="i-Globe text-16 mr-1"></i> Lives In</p><span>Dhaka, DB</span>
-						</div>
-					</div>
-					<div class="col-md-4 col-6">
-						<div class="mb-4">
-							<p class="text-primary mb-1"><i class="i-MaleFemale text-16 mr-1"></i> Gender</p><span>1 Jan, 1994</span>
-						</div>
-						<div class="mb-4">
-							<p class="text-primary mb-1"><i class="i-MaleFemale text-16 mr-1"></i> Email</p><span>example@ui-lib.com</span>
-						</div>
-						<div class="mb-4">
-							<p class="text-primary mb-1"><i class="i-Cloud-Weather text-16 mr-1"></i> Website</p><span>www.ui-lib.com</span>
+						<p class="text-primary mb-1"><i class="i-Professor text-16 mr-1"></i> Visi</p><span>{{$tenant->visi}}</span>
 						</div>
 					</div>
 					<div class="col-md-4 col-6">
 						<div class="mb-4">
-							<p class="text-primary mb-1"><i class="i-Face-Style-4 text-16 mr-1"></i> Profession</p><span>Digital Marketer</span>
+							<p class="text-primary mb-1"><i class="i-MaleFemale text-16 mr-1"></i> Contact</p><span>{{$tenant->kontak}}</span>
 						</div>
 						<div class="mb-4">
-							<p class="text-primary mb-1"><i class="i-Professor text-16 mr-1"></i> Experience</p><span>8 Years</span>
+						<p class="text-primary mb-1"><i class="i-Cloud-Weather text-16 mr-1"></i> Website</p><span>www.ui-lib.com</span>
 						</div>
 						<div class="mb-4">
-							<p class="text-primary mb-1"><i class="i-Home1 text-16 mr-1"></i> School</p><span>School of Digital Marketing</span>
+						<p class="text-primary mb-1"><i class="i-Professor text-16 mr-1"></i> Misi</p><span>{{$tenant->misi}}</span>
+						</div>
+					</div>
+					<div class="col-md-4 col-6">
+						<div class="mb-4">
+							<p class="text-primary mb-1"><i class="i-Face-Style-4 text-16 mr-1"></i> Bidang Usaha</p><span>{{$tenant->bidang_usaha}}</span>
+						</div>
+						<div class="mb-4">
+							<p class="text-primary mb-1"><i class="i-Professor text-16 mr-1"></i> Slogan</p><span>{{$tenant->slogan}}</span>
+						</div>
+						<div class="mb-4">
+						<p class="text-primary mb-1"><i class="i-Professor text-16 mr-1"></i> Jam Operasional</p><span>{{$tenant->jam_operasional}}</span>
 						</div>
 					</div>
 				</div>
